76d19410b6616d670a6e058a072388131ed518c5
Mitchell Rodríguez <mitrodle@gmail.com>
2017-05-26 12:07:32 -0500

    Places implementado

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,4 +38,35 @@ function initMap() {
   var funcionError = function(error){
     alert("Tenemos un problema con encontrar tu ubicación");
   }
+
+  var inputPartida = document.getElementById("punto-partida");
+  var inputDestino = document.getElementById("punto-destino");
+
+  new google.maps.places.Autocomplete(inputPartida);
+  new google.maps.places.Autocomplete(inputDestino);
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+
+  var calculateAndDisplayRoute = function(directionsService, directionsDisplay){
+    directionsService.route({
+      origin: inputPartida.value,
+      destination: inputDestino.value,
+      travelMode: 'DRIVING'
+    }, function(response, status){
+      if(status === 'OK'){
+        directionsDisplay.setDirections(response);
+      }else{
+        window.alert("No encontramos una ruta.");
+      }
+    });
+  }
+
+  directionsDisplay.setMap(map);
+
+  var trazarRuta = function(){
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+  };
+
+  document.getElementById("trazar-ruta").addEventListener("click", trazarRuta);
 }
